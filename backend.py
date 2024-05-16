f579ea42de4c0773d25994dab9cfb1d68f4bf39c
AlanArenas0363 <159272184+AlanArenas0363@users.noreply.github.com>
2024-05-15 20:56:15 -0600

    Add files via upload
    Actualización v0.2

--- backend.py
+++ backend.py
@@ -1,14 +1,32 @@
 import numpy as np
+import heapq #* Se agregó esta nueva librería
 
 #* Para ver los comentarios dinamicamente, instalar 'Better Comments' en Visual Studio Code
 
 #TODO: Programa a realizar acerca del problema del enrutamiento de vehiculo
-    #? ¿Que se puede utilizar?
-        #* Bases de datos:
-            #* Kaggle
-            #* Google OR-Tools
-            #* Hugging Face
+
+def main():
+    #* Entradas de texto de los puntos que debe de tomar en cuenta el usuario para resolver el problema
+    xInicio = float(input("Punto x inicial: "))
+    yInicio = float(input("Punto y inicial: "))
+    xFinal = float(input("Punto x final: "))
+    xFinal = float(input("Punto y final: "))
+
+    opti = input("Criterio (distancia, tiempo): ")
+
+    if opti == "distancia":
+        print("Eligió distancia, se agregará más adelante")
+    
+    elif opti == "tiempo":
+        print("Eligió tiempo, se agregará más adelante")
+    
+    else:
+        print("Criterio inválido")
 
 print("Funciona correctamente") #! NO ELIMINAR HASTA TERMINAR
 
-#* Revisar Avance 1 (13/Mayo/2024) para ver más información del problema :D
+if __name__ == "__main__":
+    main()
+
+#* Revisar Changelog del programa en el README.md de este repositorio
+#* Revisar contexto del problema en el README.md de este repositorio
