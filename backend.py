f9edb9a7d1acec73362e1d3f7994ead4bacc7f68
AlanArenas0363 <159272184+AlanArenas0363@users.noreply.github.com>
2024-05-18 21:06:15 -0600

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,17 +1,29 @@
 import numpy as np
-import heapq #* Se agregó esta nueva librería
+import heapq as hpq #? Se agregó esta nueva librería
+#* Instalar 'Better Comments' en Visual Studio Code 
 
-#* Para ver los comentarios dinamicamente, instalar 'Better Comments' en Visual Studio Code
+#* Recomendaciones para el usuario
+#? Que se agregó/Modificó
+#! Variables/Funciones de prueba (Se eliminarán hasta terminar el proyecto)
 
-#TODO: Programa a realizar acerca del problema del enrutamiento de vehiculo
+#? Clase grafo de prueba para el cálculo del problema, puede sufrir cambios o seguir en la version final 
+""" class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = np.zeros((vertices, vertices))
 
+    def addLimite(self, src, dest, weight):
+        self.graph[src][dest] = weight
+        self.graph[dest][src] = weight
+"""
 def main():
-    #* Entradas de texto de los puntos que debe de tomar en cuenta el usuario para resolver el problema
+    #? Entradas de texto de los puntos que debe de tomar en cuenta el usuario para resolver el problema
     xInicio = float(input("Punto x inicial: "))
     yInicio = float(input("Punto y inicial: "))
     xFinal = float(input("Punto x final: "))
     xFinal = float(input("Punto y final: "))
 
+    #? Opción de optimización para resolver el problema
     opti = input("Criterio (distancia, tiempo): ")
 
     if opti == "distancia":
